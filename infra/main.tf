67f295580fc9be50db48e829d9a51403b2d6f380
Tobi Oladipo <Tobi.Oladipo@hilti.com>
2022-10-31 15:48:54 +0100

    adding s3 backend

--- infra/main.tf
+++ infra/main.tf
@@ -2,6 +2,12 @@ terraform {
   required_providers {
     aws = "~> 4.3.0"
   }
+
+  backend "s3" {
+    bucket = "terraform-backend-busayo"
+    key    = "test/app-stack.tfstate"
+    region = "us-east-2"
+  }
 }
 
 provider "aws" {
